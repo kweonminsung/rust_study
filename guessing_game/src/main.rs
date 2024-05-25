dec82ad8575cecfcedb9562550835276ad64cb4d
kweonminsung <kevin136583@gmail.com>
2024-05-25 11:38:54 +0900

    rewrite guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,34 +1,35 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("숫자를 맞혀봅시다!");
+    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 10);
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
     loop {
-        println!("정답이라고 생각하는 숫자를 입력하세요.");
-         
+        println!("Please input your guess.");
+
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
-            .expect("입력한 값을 읽지 못했습니다");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-        let guess: i32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("입력한 값: {}", guess);
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("입력한 숫자가 작습니다!"),
-            Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("정답!");
+                println!("You win!");
                 break;
-            },
-        };
+            }
+        }
     }
 }
